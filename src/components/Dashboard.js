1409ffcb1f83a5481b4a95f3824fa838f127727d
karenok <karenokonkwo29@gmail.com>
2020-02-06 18:00:07 +0000

    feat: Welcome dashboard with styles

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import image from "../images/wave.gif"
+import "../styles/Dashboard.css";
+
+function Dashboard() {
+    return (
+        <main className="dashboard">
+            <img src={image} alt={"Girl waving"} />
+            <h1> Welcome to Softcom! </h1>
+        </main>
+    );
+
+}
+
+export default Dashboard;
