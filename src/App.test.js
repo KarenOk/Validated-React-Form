18a118f36ffc2a75add79bf319b9dfa1bf5a3216
karenok <karenokonkwo29@gmail.com>
2020-02-07 18:56:58 +0000

    Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import '@testing-library/jest-dom/extend-expect'
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+})
+
+
+
+it("renders register button", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  expect(getByText("Register")).toBeInTheDocument();
+})
